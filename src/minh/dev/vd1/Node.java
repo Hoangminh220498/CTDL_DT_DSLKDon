9b2d552459c7471c9af0830359a2644280764dc4
= <=>
2021-09-14 20:35:59 +0700

    first commit

---
+++ src/minh/dev/vd1/Node.java
@@ -0,0 +1,15 @@
+package minh.dev.vd1;
+
+public class Node {
+	public int noidung;
+	public Node next;
+	
+	public Node(int nd) { 
+		noidung = nd;
+	}
+	
+	public void printNode() {
+		System.out.printf("%d\t",noidung);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module DSLKDon {
+}
